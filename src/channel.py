aca1524a06ded0c9d3150bdc94d3528e24a7a3d9
pavel <Dante94_94@bk.ru>
2023-08-30 21:51:06 +0300

    complete homework=)

--- src/channel.py
+++ src/channel.py
@@ -11,9 +11,7 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
 
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
@@ -30,14 +28,15 @@ class Channel:
     def get_service(cls):
         api_key: str = os.getenv('YT_API_KEY')
 
-        return cls('youtube', 'v3', developerKey=api_key)
+        return build('youtube', 'v3', developerKey=api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        print(youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute())
+        print(self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute())
 
     def to_json(self, filename):
         with open(filename, 'w', encoding='utf-8') as f:
-            json.dumps(self.title, f)
+            channel_data = {'channel_id': self.__channel_id, 'title': self.title, 'description': self.description,
+                            'url': self.url, 'subscribers': self.subscribers, 'video_count': self.video_count,
+                            'view_count': self.view_count}
+            json.dump(channel_data, f, ensure_ascii=False, indent=2)
