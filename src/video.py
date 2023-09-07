9527d476231a11948170d9c608afed0599908738
pavel <Dante94_94@bk.ru>
2023-09-08 01:28:07 +0300

    Add new class

---
+++ src/video.py
@@ -0,0 +1,46 @@
+import os
+
+from googleapiclient.discovery import build
+
+
+class Video:
+    def __init__(self, video_id):
+        self.__video_id = video_id
+
+        video = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                 id=self.__video_id
+                                                 ).execute()
+
+        self.id_video: str = video['items'][0]['id']
+        self.video_title: str = video['items'][0]['snippet']['title']
+        self.url = 'https://www.youtube.com/watch/' + self.__video_id
+        self.view_count: int = video['items'][0]['statistics']['viewCount']
+        self.like_count: int = video['items'][0]['statistics']['likeCount']
+
+    @property
+    def video_id(self):
+        return self.__video_id
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+    def print_info(self):
+        print(self.get_service().videos().list(id=self.video_id, part='snippet,statistics').execute())
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.__playlist_id = playlist_id
+
+        playlist_videos = self.get_service().playlistItems().list(playlistId=self.__playlist_id,
+                                                                  part='contentDetails',
+                                                                  maxResults=50,
+                                                                  ).execute()
+        self.video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
